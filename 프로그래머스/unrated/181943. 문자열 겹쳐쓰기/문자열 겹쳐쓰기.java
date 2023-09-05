30e4e5e8271ec04c1848e23bd16f4e54dfd8b0df
김하늘 <110077343+kimskyyyy@users.noreply.github.com>
2023-09-05 16:15:01 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 0.05 ms, Memory: 68.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+       StringBuilder answer = new StringBuilder();
+
+        answer.append(my_string.substring(0, s));
+        answer.append(overwrite_string);
+        answer.append(my_string.substring(answer.length()));
+        
+        return answer.toString();
+    }
+}
